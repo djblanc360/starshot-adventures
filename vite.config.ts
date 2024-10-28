e2592890aa83e580c0f70304d782db1ba707e5bb
djblanc360 <djblanc360@gmail.com>
2024-10-28 16:43:15 -0700

    added vite-plugin-shopify and removed live reload in shopify theme dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import shopify from 'vite-plugin-shopify';
 
 // ESM equivalent of __dirname
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  plugins: [
+    // shopify({
+    //   // Options for vite-plugin-shopify
+    //   entrypointsDir: 'src',          // Directory containing entry points (e.g., main.ts and main.css)
+    //   themeRoot: __dirname,           // Root of the Shopify theme
+    // })
+  ],
   publicDir: path.resolve(__dirname, 'public'), // output directory for Vite's build
   build: {
     outDir: path.resolve(__dirname, 'assets'),  // output the build directly into Shopify's assets directory
